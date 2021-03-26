20cb6575e75e4253b99ad1ee7e7391c104297ebc
PC_diego <diegotovioak@unimagdalena.edu.co>
2021-03-26 10:11:03 -0500

    configuracion del modem ec25au para enviar mensaje MQTT

--- MCUX_FRDM_KL02Z_IoT_RTU_demo/sdk_peripherals/sdk_pph_ec25au.h
+++ MCUX_FRDM_KL02Z_IoT_RTU_demo/sdk_peripherals/sdk_pph_ec25au.h
@@ -1,3 +1,4 @@
+
 /*! @file : sdk_pph_ec25au.h
  * @author  Ernesto Andres Rincon Cruz
  * @version 1.0.0
@@ -8,19 +9,14 @@
  */
 #ifndef SDK_PPH_EC25AU_H_
 #define SDK_PPH_EC25AU_H_
+
+
 /*******************************************************************************
  * Includes
  ******************************************************************************/
 #include "sdk_hal_uart0.h"
 
-/*!
- * @addtogroup PPH
- * @{
- */
-/*!
- * @addtogroup EC25AU
- * @{
- */
+
 /*******************************************************************************
  * Public Definitions
  ******************************************************************************/
@@ -28,11 +24,23 @@ enum _ec25_lista_comandos_at {
 	kAT = 0,
 	kATI,
 	kAT_CPIN,
+	kAT_QCFG_CONFIG1,
+	kAT_QCFG_CONFIG2,
+	kAT_QCSQ,
 	kAT_CREG,
-	kAT_CMGF_1,
-	kAT_CMGS,
-	kAT_TEXT_MSG_END,
+	kAT_CGREG,
+	kAT_CEREG,
+	kAT_CGDCONT,
+	kAT_QIACT_1,
+	kAT_QIACT,
+	kAT_QMTOPEN,
+	kAT_QMTCONN,
+	kAT_QMTPUB_T_H,
+	kAT_MQTT_MSJ_T_H,
+	kAT_CFUN_0,
+	kAT_CFUN_1,
 	kAT_CSQ,
+	kAT_QLTS_2,
 };
 
 enum _fsm_ec25_state{
@@ -40,20 +48,37 @@ enum _fsm_ec25_state{
 	kFSM_ENVIANDO_AT,
 	kFSM_ENVIANDO_ATI,
 	kFSM_ENVIANDO_CPIN,
+	kFSM_ENVIANDO_QCFG_CONFIG1,
+	kFSM_ENVIANDO_QCFG_CONFIG2,
+	kFSM_ENVIANDO_QCSQ,
 	kFSM_ENVIANDO_CREG,
-	kFSM_ENVIANDO_CMGF,
-	kFSM_ENVIANDO_CMGS,
-	kFSM_ENVIANDO_MENSAJE_TXT,
+	kFSM_ENVIANDO_CGREG,
+	kFSM_ENVIANDO_CEREG,
+	kFSM_ENVIANDO_CGDCONT,
+	kFSM_ENVIANDO_QIACT_1,
+	kFSM_ENVIANDO_QIACT,
+	kFSM_ENVIANDO_QMTOPEN,
+	kFSM_ENVIANDO_QMTCONN,
+	kFSM_ENVIANDO_QMTPUB_T_H,
+	kFSM_ENVIANDO_MQTT_MSJ_T_H,
+	kFSM_ENVIANDO_CFUN_0,
+	kFSM_ENVIANDO_CFUN_1,
+	kFSM_ENVIANDO_CSQ,
+	kFSM_ENVIANDO_QLTS_2,
 	kFSM_ESPERANDO_RESPUESTA,
 	kFSM_RESULTADO_ERROR,
 	kFSM_RESULTADO_EXITOSO,
-	kFSM_ENVIANDO_CSQ,
 	kFSM_PROCESANDO_RESPUESTA,
 	kFSM_RESULTADO_ERROR_RSSI,
+	kFSM_RESULTADO_ERROR_QMTOPEN,
+	kFSM_RESULTADO_ERROR_QIACT_1,
+	kFSM_RESULTADO_ERROR_QMTPUB_T_H,
 };
 
-#define EC25_TIEMPO_MAXIMO_ESPERA	3		//Tiempo maximo que espera modem por respuesta
-#define EC25_RSSI_MINIMO_ACEPTADO	20		//RSSI minimo aceptado segun tabla de fabricante
+#define EC25_TIEMPO_MAXIMO_ESPERA	5		//Tiempo maximo que espera modem por respuesta
+#define EC25_RSSI_MINIMO_ACEPTADO	12		//RSSI minimo aceptado segun tabla del fabricante
+
+
 /*******************************************************************************
  * External vars
  ******************************************************************************/
@@ -66,10 +91,9 @@ enum _fsm_ec25_state{
  * Public Prototypes
  ******************************************************************************/
 status_t ec25Inicializacion(void);
-status_t ec25EnviarMensajeDeTexto(uint8_t *mensaje, uint8_t size_mensaje );
+status_t ec25InicializarMQTT(void);
+status_t ec25sensor(float valor_temp, float valor_hum);
 uint8_t ec25Polling(void);
 
-/** @} */ // end of X group
-/** @} */ // end of X group
 
 #endif /* SDK_PPH_EC25AU_H_ */
